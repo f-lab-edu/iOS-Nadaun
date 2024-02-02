8add4d668077a9fe26bfc60f6e3e01c87fe784fb
leegyoungmin <cow970814@naver.com>
2024-02-02 23:18:58 +0900

    config : 시작 화면 구현

--- iOS-Naduan/Source/ViewController.swift
+++ iOS-Naduan/Source/ViewController.swift
@@ -3,17 +3,13 @@
 //  iOS-Naduan
 //
 //  Copyright (c) 2024 Minii All rights reserved.
-        
 
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .red
   }
-
-
 }
 
