6016b177d84c9f08a72735168f3973346e31f9da
leegyoungmin <cow970814@naver.com>
2024-02-02 15:35:06 +0900

    feat : Start Project

---
+++ Nadaun/Nadaun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nadaun
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+        
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
