a23bfff61e5f91aae523cdfa623eae757f24be7d
leegyoungmin <cow970814@naver.com>
2024-02-03 17:34:31 +0900

    feat : AuthController 와 AuthControllerDelegate 메서드 구현

--- iOS-Naduan/Source/Presentation/AuthScene/LoginView/Views/AuthController.swift
+++ iOS-Naduan/Source/Presentation/AuthScene/LoginView/Views/AuthController.swift
@@ -8,6 +8,13 @@ import AuthenticationServices
 
 import KakaoSDKUser
 
+protocol AuthControllerDelegate: AnyObject {
+  func authController(to controller: AuthController, didSuccess idToken: String)
+  func authController(to controller: AuthController, didFailure error: Error)
+}
+
 class AuthController {
+  weak var delegate: AuthControllerDelegate?
+  
   
 }
