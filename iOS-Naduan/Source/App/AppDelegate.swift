50ff9833e484d418bbbc9927aa2638ebf1c48d4e
leegyoungmin <cow970814@naver.com>
2024-02-03 17:13:30 +0900

    config : Kakao SDK 초기화 메서드 추가

--- iOS-Naduan/Source/App/AppDelegate.swift
+++ iOS-Naduan/Source/App/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
+    KakaoSDK.initSDK(appKey: Bundle.KAKAO_KEY())
     return true
   }
   
