70227617b6cb4a75f0f7442ef5dc7c36cb368e64
leegyoungmin <cow970814@naver.com>
2024-02-03 17:37:53 +0900

    feat : AuthController Kakao 로그인 메서드 구현

--- iOS-Naduan/Source/Models/AuthModels/AuthError.swift
+++ iOS-Naduan/Source/Models/AuthModels/AuthError.swift
@@ -5,5 +5,6 @@
 //  Copyright (c) 2024 Minii All rights reserved.
 
 enum AuthError: Error {
+  case didNotFoundIDToken
   case didNotFoundUser
 }
--- iOS-Naduan/Source/Presentation/AuthScene/LoginView/Views/AuthController.swift
+++ iOS-Naduan/Source/Presentation/AuthScene/LoginView/Views/AuthController.swift
@@ -7,6 +7,7 @@
 import AuthenticationServices
 
 import KakaoSDKUser
+import KakaoSDKAuth
 
 protocol AuthControllerDelegate: AnyObject {
   func authController(to controller: AuthController, didSuccess idToken: String)
@@ -16,5 +17,25 @@ protocol AuthControllerDelegate: AnyObject {
 class AuthController {
   weak var delegate: AuthControllerDelegate?
   
+  func authWithKakao() {
+    if UserApi.isKakaoTalkLoginAvailable() {
+      UserApi.shared.loginWithKakaoTalk(completion: handleKakaoToken)
+    } else {
+      UserApi.shared.loginWithKakaoAccount(completion: handleKakaoToken)
+    }
+  }
   
+  private func handleKakaoToken(_ token: OAuthToken?, _ error: Error?) {
+    if let error = error {
+      delegate?.authController(to: self, didFailure: error)
+      return
+    }
+    
+    guard let idToken = token?.idToken else {
+      delegate?.authController(to: self, didFailure: AuthError.didNotFoundIDToken)
+      return
+    }
+    
+    delegate?.authController(to: self, didSuccess: idToken)
+  }
 }
