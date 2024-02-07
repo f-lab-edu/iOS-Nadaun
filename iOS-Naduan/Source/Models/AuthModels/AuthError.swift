bad70e5c564fb09424f29f11435b6048ce77f4a7
leegyoungmin <cow970814@naver.com>
2024-02-07 16:30:29 +0900

    refactor : AuthError 각 케이스 네이밍 변경

--- iOS-Naduan/Source/Models/AuthModels/AuthError.swift
+++ iOS-Naduan/Source/Models/AuthModels/AuthError.swift
@@ -6,21 +6,9 @@
 
 import Foundation
 
-enum AuthError: Error, LocalizedError {
-  case didNotFoundCredential
-  case didNotFoundIDToken
-  case didNotFoundUser
+enum AuthError: Error {
+  case credentialMissing
+  case IDTokenMissing
+  case userMissing
   case unexpected
-  
-  var failureReason: String? {
-    return "예기치 못한 에러가 발생하였습니다."
-  }
-  
-  var recoverySuggestion: String? {
-    return "잠시후 다시 시도해주세요."
-  }
-  
-  var errorDescription: String? {
-    return (failureReason ?? "") + "\n" + (recoverySuggestion ?? "")
-  }
 }
