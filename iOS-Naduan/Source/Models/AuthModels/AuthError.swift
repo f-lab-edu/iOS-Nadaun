1c03b41319bcd2e03514fced41deb708ff749b28
leegyoungmin <cow970814@naver.com>
2024-02-07 16:44:35 +0900

    refactor : AuthError LocalizedError 이유 수정

--- iOS-Naduan/Source/Models/AuthModels/AuthError.swift
+++ iOS-Naduan/Source/Models/AuthModels/AuthError.swift
@@ -6,9 +6,24 @@
 
 import Foundation
 
-enum AuthError: Error {
+enum AuthError {
   case credentialMissing
   case IDTokenMissing
   case userMissing
   case unexpected
+}
+
+extension AuthError: LocalizedError {
+  var failureReason: String? {
+    switch self {
+      case .credentialMissing:
+        return "자격증명을 획득하지 못했습니다."
+      case .IDTokenMissing:
+        return "토큰을 획득하지 못했습니다."
+      case .userMissing:
+        return "사용자를 찾지 못했습니다."
+      case .unexpected:
+        return "예기치 못한 에러가 발생하였습니다."
+    }
+  }
 }
