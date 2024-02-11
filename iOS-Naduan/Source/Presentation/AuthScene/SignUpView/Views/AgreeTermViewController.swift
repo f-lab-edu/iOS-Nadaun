60af97352ce11d7c05c3e28264c318c82a859322
leegyoungmin <cow970814@naver.com>
2024-02-11 16:23:12 +0900

    feat : Sign Up 자식 VC 모두 생성

---
+++ iOS-Naduan/Source/Presentation/AuthScene/SignUpView/Views/AgreeTermViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AgreeTermViewController.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+import UIKit
+
+class AgreeTermViewController: UIViewController {
+  weak var signUpDelegate: SignUpFlowChildControllerDelegate?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .red
+  }
+}
---
+++ iOS-Naduan/Source/Presentation/AuthScene/SignUpView/Views/SettingProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingProfileViewController.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+import UIKit
+
+final class SettingProfileViewController: UIViewController {
+  weak var signUpDelegate: SignUpFlowChildControllerDelegate?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBlue
+  }
+}
