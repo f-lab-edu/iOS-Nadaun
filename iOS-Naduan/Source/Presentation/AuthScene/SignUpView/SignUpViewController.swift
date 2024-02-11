29ca908aa377669a8fa0323fe9ef78249e595ac5
leegyoungmin <cow970814@naver.com>
2024-02-11 15:47:26 +0900

    feat : Sign Up ViewController 관련 화면 생성 및 전환 구현

---
+++ iOS-Naduan/Source/Presentation/AuthScene/SignUpView/SignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewController.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+import UIKit
+
+final class SignUpViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBackground
+  }
+}
