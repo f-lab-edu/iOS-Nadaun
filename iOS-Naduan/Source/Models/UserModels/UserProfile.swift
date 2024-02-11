6073f5146aecde43f023f4b2617e8d78c9167899
leegyoungmin <cow970814@naver.com>
2024-02-11 15:43:11 +0900

    feat : User 관련 모델 생성

---
+++ iOS-Naduan/Source/Models/UserModels/UserProfile.swift
@@ -0,0 +1,12 @@
+//
+//  UserProfile.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+struct UserProfile {
+  var nickName: String?
+  var email: String?
+  var phoneNumber: String?
+  var position: String?
+}
