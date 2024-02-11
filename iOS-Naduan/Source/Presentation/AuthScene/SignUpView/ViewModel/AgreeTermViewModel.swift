0d5c001b2fb0a0fa9417e95174c720c506d2b17f
leegyoungmin <cow970814@naver.com>
2024-02-12 00:11:49 +0900

    feat : AgreeTerm ViewModel 생성

---
+++ iOS-Naduan/Source/Presentation/AuthScene/SignUpView/ViewModel/AgreeTermViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  AgreeTermViewModel.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+class AgreeTermViewModel {
+  enum AgreeTermAction {
+    case selectTerm
+    case selectPrivacy
+  }
+  
+  private var isSelectTerm: Bool = false {
+    didSet {
+      isSelectAll?(isSelectTerm && isSelectPrivacy)
+    }
+  }
+  
+  private var isSelectPrivacy: Bool = false {
+    didSet {
+      isSelectAll?(isSelectTerm && isSelectPrivacy)
+    }
+  }
+  
+  var isSelectAll: ((Bool) -> Void)?
+  
+  private func isAllSelect() -> Bool {
+    return isSelectTerm && isSelectPrivacy
+  }
+  
+  func bind(action: AgreeTermAction) {
+    switch action {
+      case .selectTerm:
+        isSelectTerm = (isSelectTerm == true) ? false : true
+      case .selectPrivacy:
+        isSelectPrivacy = (isSelectPrivacy == true) ? false : true
+    }
+  }
+}
