3c751b9a57a8a19717c813843bdf7465fb40d1d0
leegyoungmin <cow970814@naver.com>
2024-02-11 16:08:11 +0900

    feat : Sign Up 관련 다음 버튼 구현

--- iOS-Naduan/Source/Presentation/AuthScene/SignUpView/SignUpViewController.swift
+++ iOS-Naduan/Source/Presentation/AuthScene/SignUpView/SignUpViewController.swift
@@ -6,10 +6,70 @@
 
 import UIKit
 
-final class SignUpViewController: UIViewController {
+enum SignUpFlow {
+  case agreeTerm
+  case settingProfile
+  case generateBasicCard
+}
+
+final class SignUpViewController: UITabBarController {
+  private let nextFlowButton: UIButton = {
+    var attributes = AttributeContainer()
+    attributes.font = UIFont.pretendardFont(weight: .bold, size: 18)
+    
+    var configuration = UIButton.Configuration.filled()
+    configuration.baseBackgroundColor = UIColor.accent
+    configuration.background.cornerRadius = .zero
+    configuration.attributedTitle = AttributedString("다음", attributes: attributes)
+    configuration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: .zero, bottom: 40, trailing: .zero)
+    
+    let button = UIButton(configuration: configuration)
+    button.automaticallyUpdatesConfiguration = false
+    
+    button.configurationUpdateHandler = { button in
+      switch button.state {
+        case .disabled:
+          button.configuration?.background.backgroundColor = .gray
+        case .normal:
+          button.configuration?.background.backgroundColor = .accent
+        default:
+          return
+      }
+    }
+    return button
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    configureTabBar()
+    configureUI()
+  }
+}
+
+private extension SignUpViewController {
+  func configureTabBar() {
+    tabBar.isHidden = true
+  }
+}
+
+private extension SignUpViewController {
+  func configureUI() {
     view.backgroundColor = .systemBackground
+    
+    configureHierarchy()
+    makeConstraints()
+  }
+  
+  func configureHierarchy() {
+    [nextFlowButton].forEach(view.addSubview)
+  }
+  
+  func makeConstraints() {
+    nextFlowButton.attach {
+      $0.leading(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+      $0.trailing(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+      $0.bottom(equalTo: view.bottomAnchor)
+    }
   }
 }
