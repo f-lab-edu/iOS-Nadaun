7d5f09d8ed84ea9729980bea7d3f275c8d6adec3
leegyoungmin <cow970814@naver.com>
2024-02-11 22:54:16 +0900

    feat : UITableView Cell 편의 메서드 구현

---
+++ iOS-Naduan/Source/Extensions/Reusable + Extension.swift
@@ -0,0 +1,29 @@
+//
+//  UITableView + Extension.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+import UIKit
+
+protocol Reusable: AnyObject {
+  static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+  static var reuseIdentifier: String {
+    return String(describing: Self.self)
+  }
+}
+
+extension UITableViewCell: Reusable { }
+
+extension UITableView {
+  func dequeueReusableCell<T: UITableViewCell>(with indexPath: IndexPath) -> T {
+    guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+      return T()
+    }
+    
+    return cell
+  }
+}
