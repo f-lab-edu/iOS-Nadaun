fed1949351678fe1554b300be55abfca80454594
leegyoungmin <cow970814@naver.com>
2024-02-11 15:43:11 +0900

    feat : User 관련 모델 생성

---
+++ iOS-Naduan/Source/Models/UserModels/BusinessCard.swift
@@ -0,0 +1,11 @@
+//
+//  BusinessCard.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+struct BusinessCard {
+  let profile: UserProfile
+  var companyName: String?
+  var companyAddress: String?
+}
