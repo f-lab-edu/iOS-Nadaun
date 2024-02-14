ce9ecea127ebd2434774af5dd49761e7ac6df585
leegyoungmin <cow970814@naver.com>
2024-02-14 15:52:29 +0900

    refactor : Button Style Method 통합으로 변경

--- iOS-Naduan/Source/Presentation/AuthScene/SignUpView/Controllers/SettingProfileViewController.swift
+++ iOS-Naduan/Source/Presentation/AuthScene/SignUpView/Controllers/SettingProfileViewController.swift
@@ -19,42 +19,34 @@ final class SettingProfileViewController: UIViewController {
     return stackView
   }()
   
-  override init(title: String) {
-    super.init(title: title)
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     
   }
   
-  override func configureHierarchy() {
-    super.configureHierarchy()
-    
-    [profileInputScrollView].forEach(view.addSubview)
-    [scrollContentView].forEach(profileInputScrollView.addSubview)
-  }
-  
-  override func makeConstraints() {
-    super.makeConstraints()
-    
-    profileInputScrollView.attach {
-      $0.top(equalTo: titleLabel.bottomAnchor, padding: 12)
-      $0.leading(equalTo: titleLabel.leadingAnchor)
-      $0.trailing(equalTo: titleLabel.trailingAnchor)
-      $0.bottom(equalTo: nextFlowButton.topAnchor, padding: 12)
-    }
-    
-    scrollContentView.attach {
-      $0.top(equalTo: profileInputScrollView.contentLayoutGuide.topAnchor)
-      $0.leading(equalTo: profileInputScrollView.contentLayoutGuide.leadingAnchor)
-      $0.trailing(equalTo: profileInputScrollView.contentLayoutGuide.trailingAnchor)
-      $0.bottom(equalTo: profileInputScrollView.contentLayoutGuide.bottomAnchor)
-      $0.width(equalTo: profileInputScrollView.frameLayoutGuide.widthAnchor)
-    }
-  }
+//  override func configureHierarchy() {
+//    super.configureHierarchy()
+//    
+//    [profileInputScrollView].forEach(view.addSubview)
+//    [scrollContentView].forEach(profileInputScrollView.addSubview)
+//  }
+//  
+//  override func makeConstraints() {
+//    super.makeConstraints()
+//    
+//    profileInputScrollView.attach {
+//      $0.top(equalTo: titleLabel.bottomAnchor, padding: 12)
+//      $0.leading(equalTo: titleLabel.leadingAnchor)
+//      $0.trailing(equalTo: titleLabel.trailingAnchor)
+//      $0.bottom(equalTo: nextFlowButton.topAnchor, padding: 12)
+//    }
+//    
+//    scrollContentView.attach {
+//      $0.top(equalTo: profileInputScrollView.contentLayoutGuide.topAnchor)
+//      $0.leading(equalTo: profileInputScrollView.contentLayoutGuide.leadingAnchor)
+//      $0.trailing(equalTo: profileInputScrollView.contentLayoutGuide.trailingAnchor)
+//      $0.bottom(equalTo: profileInputScrollView.contentLayoutGuide.bottomAnchor)
+//      $0.width(equalTo: profileInputScrollView.frameLayoutGuide.widthAnchor)
+//    }
+//  }
 }
