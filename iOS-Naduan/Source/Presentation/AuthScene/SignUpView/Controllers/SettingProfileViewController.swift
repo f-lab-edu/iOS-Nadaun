057cbda0c6e71f4d85c80e32b8ac53063a43ca0f
leegyoungmin <cow970814@naver.com>
2024-02-16 15:41:19 +0900

    feat : Name TextField 추가

--- iOS-Naduan/Source/Presentation/AuthScene/SignUpView/Controllers/SettingProfileViewController.swift
+++ iOS-Naduan/Source/Presentation/AuthScene/SignUpView/Controllers/SettingProfileViewController.swift
@@ -10,36 +10,6 @@ protocol SettingProfileDelegate: AnyObject {
   func settingProfile(to controller: UIViewController, didSuccessUpdate profile: UserProfile)
 }
 
-class InsetTextField: UITextField {
-  private let padding: UIEdgeInsets
-  
-  init(padding: UIEdgeInsets) {
-    self.padding = padding
-    super.init(frame: .zero)
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override func textRect(forBounds bounds: CGRect) -> CGRect {
-    insetTextRect(for: bounds)
-  }
-  
-  override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-    insetTextRect(for: bounds)
-  }
-  
-  override func editingRect(forBounds bounds: CGRect) -> CGRect {
-    insetTextRect(for: bounds)
-  }
-  
-  private func insetTextRect(for bounds: CGRect) -> CGRect {
-    let inset = bounds.inset(by: padding)
-    return inset
-  }
-}
-
 final class SettingProfileViewController: UIViewController {
   // View Property
   private let titleLabel: UILabel = {
