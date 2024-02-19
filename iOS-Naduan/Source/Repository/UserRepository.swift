d941a719841327cb9d373032a01889645ea90777
leegyoungmin <cow970814@naver.com>
2024-02-19 16:12:47 +0900

    feat : UserRepository Email 및 데이터 저장 메서드 구현

--- iOS-Naduan/Source/Repository/UserRepository.swift
+++ iOS-Naduan/Source/Repository/UserRepository.swift
@@ -17,16 +17,70 @@ class UserRepository {
   }
   
   func updateUserProfile(to profile: UserProfile, completion: @escaping (Result<Void, Error>) -> Void) {
-    do {
-      try store.collection("User").document(user.uid)
-        .setData(from: profile)
+    let profileUpdateGroup = DispatchGroup()
+    
+    var updateError: Error? = nil
+    
+    updateEmail(to: profile.email, in: profileUpdateGroup) { result in
+      if case let .failure(error) = result {
+        updateError = error
+      }
+    }
+    
+    uploadProfile(to: profile, in: profileUpdateGroup) { result in
+      if case let .failure(error) = result {
+        updateError = error
+      }
+    }
+    
+    profileUpdateGroup.notify(queue: .global()) {
+      if let updateError = updateError {
+        completion(.failure(updateError))
+        return
+      }
+      
       completion(.success(()))
-    } catch let error {
-      completion(.failure(error))
     }
   }
   
   func updateBusinessCard(completion: @escaping (Result<Void, Error>) -> Void) {
     
+  }
+}
+
+private extension UserRepository {
+  func updateEmail(to email: String?, in group: DispatchGroup, completion: @escaping (Result<Void, Error>) -> Void) {
+    group.enter()
+    
+    guard let email = email else {
+      group.leave()
+      completion(.failure(AuthError.unexpected))
+      return
+    }
+    
+    user.updateEmail(to: email) { error in
+      if let error = error {
+        group.leave()
+        completion(.failure(error))
+        return
+      }
+      
+      group.leave()
+      completion(.success(()))
+    }
+  }
+  
+  func uploadProfile(to profile: UserProfile, in group: DispatchGroup, completion: @escaping (Result<Void, Error>) -> Void) {
+    group.enter()
+    
+    do {
+      try store.collection("User").document(user.uid).setData(from: profile)
+      
+      group.leave()
+      completion(.success(()))
+    } catch let error {
+      group.leave()
+      completion(.failure(error))
+    }
   }
 }
