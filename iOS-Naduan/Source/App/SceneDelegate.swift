bed0dd39dca471837208d3f03c51a331d131ecff
leegyoungmin <cow970814@naver.com>
2024-03-03 15:37:22 +0900

    feat : SplashViewController 생성 Constants 구현

--- iOS-Naduan/Source/App/SceneDelegate.swift
+++ iOS-Naduan/Source/App/SceneDelegate.swift
@@ -15,9 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
-    let storyBoard = UIStoryboard(name: "SplashScreen", bundle: nil)
-    let initialViewController = storyBoard.instantiateViewController(identifier: "SplashViewController")
+    let storyBoard = UIStoryboard(name: Constants.storyBoardName, bundle: nil)
+    let initialViewController = storyBoard.instantiateViewController(identifier: Constants.viewIdentifier)
     window?.rootViewController = initialViewController
     window?.makeKeyAndVisible()
+  }
+  
+  private enum Constants {
+    static let storyBoardName: String = "SplashScreen"
+    static let viewIdentifier: String = "SplashViewController"
   }
 }
