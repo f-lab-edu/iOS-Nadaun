d11ef2e439cfc938b958d7e36dd5e9932493dea0
이경민 <cow970814@naver.com>
2024-03-06 22:40:40 +0900

    feat : Main TabBar Controller TabBar 구성

--- iOS-Naduan/Source/Presentation/MainScene/MainTabBarController.swift
+++ iOS-Naduan/Source/Presentation/MainScene/MainTabBarController.swift
@@ -6,49 +6,46 @@
 
 import UIKit
 
-import FirebaseAuth
-
 final class MainTabBarController: UITabBarController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  private enum SceneRouter: CaseIterable {
+    case myCard
+    case example
     
-    view.backgroundColor = .systemBackground
-    
-    let controller = MainSampleViewController()
-    controller.tabBarItem = UITabBarItem(title: "홈", image: .checkmark, tag: .zero)
-    setViewControllers([controller], animated: true)
-  }
-}
-
-class MainSampleViewController: UIViewController {
-  private let button: UIButton = {
-    let button = UIButton(type: .system)
-    button.setTitle("로그아웃", for: .normal)
-    button.setTitleColor(.black, for: .normal)
-    return button
-  }()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    view.backgroundColor = .systemBackground
-    view.addSubview(button)
-    
-    button.attach {
-      $0.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-      $0.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-      $0.width(equalTo: 100)
+    func generateInstance() -> UIViewController {
+      switch self {
+        case .myCard:
+          let controller = MyCardViewController()
+          let tabBarItem = UITabBarItem()
+          tabBarItem.image = .add.withBaselineOffset(fromBottom: UIFont.systemFontSize * 2)
+          controller.tabBarItem = tabBarItem
+          return controller
+          
+        case .example:
+          let controller = MyCardViewController()
+          controller.tabBarItem = UITabBarItem(title: nil, image: .remove, tag: 1)
+          return controller
+      }
     }
   }
   
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    let action = UIAction { _ in
-      try? Auth.auth().signOut()
-      self.view.sceneDelegate?.presentLogin()
-    }
+    configureTabBar()
+    configureTabItems()
+  }
+  
+  private func configureTabItems() {
+    view.backgroundColor = .systemBackground
     
-    button.addAction(action, for: .touchUpInside)
+    let controllers = SceneRouter.allCases.map { $0.generateInstance() }
+    setViewControllers(controllers, animated: true)
+  }
+  
+  private func configureTabBar() {
+    tabBar.isTranslucent = false
+    tabBar.unselectedItemTintColor = .black
+    tabBar.backgroundColor = .accent
+    tabBar.tintColor = .white
   }
 }
---
+++ iOS-Naduan/Source/Presentation/MyCardScene/MyCardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyCardViewController.swift
+//  iOS-Naduan
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+import UIKit
+
+final class MyCardViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBackground
+  }
+}
