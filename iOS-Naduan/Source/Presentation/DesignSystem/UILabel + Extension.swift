6b17c5ef4ccc1046d2d9b4ae18fb31c3a009af6f
이경민 <cow970814@naver.com>
2024-03-18 15:28:27 +0900

    feat : Padding 설정할 수 있는 Label 타입 구현

--- iOS-Naduan/Source/Presentation/DesignSystem/UILabel + Extension.swift
+++ iOS-Naduan/Source/Presentation/DesignSystem/UILabel + Extension.swift
@@ -23,3 +23,27 @@ extension UILabel {
     self.attributedText = attributeString
   }
 }
+
+class Label: UILabel {
+  private let padding: UIEdgeInsets
+  
+  init(padding: UIEdgeInsets) {
+    self.padding = padding
+    super.init(frame: .zero)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func drawText(in rect: CGRect) {
+    super.drawText(in: rect.inset(by: padding))
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    var contentSize = super.intrinsicContentSize
+    contentSize.width += padding.left + padding.right
+    contentSize.height += padding.top + padding.bottom
+    return contentSize
+  }
+}
