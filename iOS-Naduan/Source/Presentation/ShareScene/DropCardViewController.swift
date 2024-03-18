939b2d1c66e883dbdc780c0fcad972e7489cb0b6
이경민 <cow970814@naver.com>
2024-03-18 15:27:44 +0900

    feat : Drop Card UI 구성

--- iOS-Naduan/Source/Presentation/ShareScene/DropCardViewController.swift
+++ iOS-Naduan/Source/Presentation/ShareScene/DropCardViewController.swift
@@ -8,7 +8,39 @@ import UIKit
 
 import Lottie
 
+class Label: UILabel {
+  private let padding: UIEdgeInsets
+  
+  init(padding: UIEdgeInsets) {
+    self.padding = padding
+    super.init(frame: .zero)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func drawText(in rect: CGRect) {
+    super.drawText(in: rect.inset(by: padding))
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    var contentSize = super.intrinsicContentSize
+    contentSize.width += padding.left + padding.right
+    contentSize.height += padding.top + padding.bottom
+    return contentSize
+  }
+}
+
 class DropCardViewController: UIViewController {
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .pretendardFont(to: .H2B)
+    label.textColor = .accent
+    label.text = "핸드폰 뒷면을 마주쳐보세요."
+    return label
+  }()
+  
   private let animationBackgroundView: LottieAnimationView = {
     let view = LottieAnimationView(name: "drop")
     view.backgroundBehavior = .continuePlaying
@@ -21,6 +53,18 @@ class DropCardViewController: UIViewController {
     let imageView = UIImageView(image: .handShake)
     imageView.contentMode = .scaleAspectFit
     return imageView
+  }()
+  
+  private let explanationLabel: Label = {
+    let padding = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    let label = Label(padding: padding)
+    label.numberOfLines = .zero
+    label.textColor = .disable
+    label.text = TextConstants.shareExplanation
+    label.font = .pretendardFont(to: .C1M)
+    label.layer.cornerRadius = 8
+    label.layer.backgroundColor = UIColor.gray01.cgColor
+    return label
   }()
   
   override func viewDidLoad() {
@@ -40,10 +84,15 @@ private extension DropCardViewController {
   }
   
   func configureHierarchy() {
-    [animationBackgroundView, shareImage].forEach(view.addSubview)
+    [titleLabel, animationBackgroundView, shareImage, explanationLabel].forEach(view.addSubview)
   }
   
   func makeConstraints() {
+    titleLabel.attach {
+      $0.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+      $0.top(equalTo: view.safeAreaLayoutGuide.topAnchor, padding: 80)
+    }
+    
     animationBackgroundView.attach {
       $0.centerXAnchor.constraint(equalTo: view.centerXAnchor)
       $0.top(equalTo: view.safeAreaLayoutGuide.topAnchor)
@@ -57,5 +106,19 @@ private extension DropCardViewController {
       $0.centerXAnchor.constraint(equalTo: view.centerXAnchor)
       $0.height(equalTo: 250)
     }
+    
+    explanationLabel.attach {
+      $0.leading(equalTo: view.leadingAnchor, padding: 24)
+      $0.trailing(equalTo: view.trailingAnchor, padding: 24)
+      $0.bottom(equalTo: view.safeAreaLayoutGuide.bottomAnchor, padding: 60)
+    }
+  }
+}
+
+private extension DropCardViewController {
+  enum TextConstants {
+    static let shareExplanation: String = """
+    해당 기능은 제한적으로 지원하고 있습니다. 지원되지 않는 기기는 QR 코드 화면이 나옵니다. 두 기기 모두 화면에 머물러야 공유가 됩니다.
+    """
   }
 }
