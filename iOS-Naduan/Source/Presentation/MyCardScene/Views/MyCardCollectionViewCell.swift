e5f65f397fda53acc8adb0d5dcace048bcded0a5
이경민 <cow970814@naver.com>
2024-03-19 14:26:38 +0900

    feat : CollectionView Cell Pan Gesture로 공유 이벤트 변경

--- iOS-Naduan/Source/Presentation/MyCardScene/Views/MyCardCollectionViewCell.swift
+++ iOS-Naduan/Source/Presentation/MyCardScene/Views/MyCardCollectionViewCell.swift
@@ -11,157 +11,13 @@ protocol MyCardCollectionViewCellDelegate: AnyObject {
   func myCardCollectionViewCell(_ cell: MyCardCollectionViewCell, didSelectShare card: BusinessCard)
 }
 
-class CardView: UIView {
-  // MARK: - View Properties
-  private let nameLabel: UILabel = {
-    let label = UILabel()
-    label.font = .pretendardFont(to: .H1B)
-    label.textColor = .title
-    label.numberOfLines = 1
-    return label
-  }()
-  
-  private let positionLabel: UILabel = {
-    let label = UILabel()
-    label.font = .pretendardFont(to: .B3M)
-    label.textColor = .body1
-    label.numberOfLines = 1
-    return label
-  }()
-  
-  private let companyLabel: UILabel = {
-    let label = UILabel()
-    label.font = .pretendardFont(to: .C2R)
-    label.textColor = .body1
-    label.numberOfLines = 1
-    return label
-  }()
-  
-  private let phoneStackView = generatePrivacyStackView()
-  
-  private let phoneImage: UIImageView = {
-    let imageView = UIImageView(image: .phone)
-    return imageView
-  }()
-  
-  private let phoneLabel: UILabel = {
-    let label = UILabel()
-    label.font = .pretendardFont(to: .C2R)
-    label.textColor = .body2
-    label.textAlignment = .right
-    return label
-  }()
-  
-  private let emailStackView = generatePrivacyStackView()
-  
-  private let emailImage: UIImageView = {
-    let imageView = UIImageView(image: .email)
-    return imageView
-  }()
-  
-  private let emailLabel: UILabel = {
-    let label = UILabel()
-    label.font = .pretendardFont(to: .C2R)
-    label.textColor = .body2
-    label.textAlignment = .right
-    return label
-  }()
-  
-  init() {
-    super.init(frame: .zero)
-    
-    configureUI()
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  static func generatePrivacyStackView() -> UIStackView {
-    let stackView = UIStackView()
-    stackView.alignment = .fill
-    stackView.distribution = .equalSpacing
-    stackView.spacing = 10
-    return stackView
-  }
-  
-  func reset() {
-    nameLabel.text = nil
-    positionLabel.text = nil
-    companyLabel.text = nil
-    emailLabel.text = nil
-    phoneLabel.text = nil
-    phoneStackView.isHidden = true
-  }
-  
-  func bind(to card: BusinessCard) {
-    nameLabel.text = card.name
-    positionLabel.text = card.position
-    companyLabel.text = card.companyDescription
-    emailLabel.text = card.email
-    
-    if let phone = card.phone {
-      phoneLabel.text = phone
-      phoneStackView.isHidden = false
-    }
-  }
-}
-// MARK: - Configure UI Methods
-private extension CardView {
-  func configureUI() {
-    configureHierarchy()
-    makeConstraints()
-  }
-  
-  func configureHierarchy() {
-    [phoneImage, phoneLabel].forEach(phoneStackView.addArrangedSubview)
-    [emailImage, emailLabel].forEach(emailStackView.addArrangedSubview)
-    [
-      nameLabel, positionLabel, companyLabel,
-      phoneStackView, emailStackView
-    ].forEach { addSubview($0) }
-  }
-  
-  func makeConstraints() {
-    nameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-    
-    nameLabel.attach {
-      $0.top(equalTo: topAnchor, padding: 16)
-      $0.leading(equalTo: leadingAnchor, padding: 16)
-      $0.trailing(equalTo: trailingAnchor, padding: 16)
-    }
-    
-    positionLabel.attach {
-      $0.top(equalTo: nameLabel.bottomAnchor, padding: 4)
-      $0.leading(equalTo: nameLabel.leadingAnchor)
-      $0.trailing(equalTo: trailingAnchor, padding: 16)
-    }
-    
-    companyLabel.attach {
-      $0.top(equalTo: positionLabel.bottomAnchor, padding: 4)
-      $0.leading(equalTo: positionLabel.leadingAnchor)
-      $0.trailing(equalTo: positionLabel.trailingAnchor)
-    }
-    
-    emailStackView.attach {
-        $0.leading(equalTo: leadingAnchor, padding: 16)
-        $0.trailing(equalTo: trailingAnchor, padding: 16)
-        $0.bottom(equalTo: bottomAnchor, padding: 16)
-    }
-    
-    phoneStackView.attach {
-      $0.leading(equalTo: leadingAnchor, padding: 16)
-      $0.trailing(equalTo: trailingAnchor, padding: 16)
-      $0.bottom(equalTo: emailStackView.topAnchor, padding: 10)
-    }
-  }
-}
-
 // MARK: MyCardCollectionViewCell
 class MyCardCollectionViewCell: UICollectionViewCell {
   // MARK: - View Properties
   private let cardView = CardView()
   
+  private var card: BusinessCard?
+  private var gesture: UIPanGestureRecognizer?
   weak var delegate: MyCardCollectionViewCellDelegate?
   
   // MARK: - Initializer
@@ -183,7 +39,48 @@ class MyCardCollectionViewCell: UICollectionViewCell {
   
   // MARK: - Binding Method
   func bind(with card: BusinessCard) {
+    self.card = card
     cardView.bind(to: card)
+    
+    let cardGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureHandler))
+    gesture = cardGesture
+    gesture?.delegate = self
+    addGestureRecognizer(cardGesture)
+  }
+  
+  @objc private func panGestureHandler(_ sender: UIPanGestureRecognizer) {
+    switch sender.state {
+      case .changed:
+        let translation = sender.translation(in: self)
+        
+        if translation.y > .zero { 
+          sender.setTranslation(.zero, in: self)
+          return
+        }
+        
+        let changedY = self.center.y + translation.y
+        self.center = CGPoint(x: center.x, y: changedY)
+        sender.setTranslation(.zero, in: self)
+      case .ended:
+        guard let superview = superview, let card = card else { return }
+        
+        let isSendCard = (frame.minY + 100) < .zero
+        
+        UIView.animate(withDuration: 0.2) { [weak self] in
+          self?.frame.origin.y = superview.frame.minY
+        }
+        
+        self.delegate?.myCardCollectionViewCell(self, didSelectShare: card)
+        
+      default: break
+    }
+  }
+}
+
+extension MyCardCollectionViewCell: UIGestureRecognizerDelegate {
+  override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    guard let gesture = gesture else { return false }
+    return gesture.velocity(in: self).y.magnitude > gesture.velocity(in: self).x.magnitude
   }
 }
 
