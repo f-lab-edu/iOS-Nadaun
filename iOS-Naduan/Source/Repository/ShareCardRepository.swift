093cd5f2ec0652fd47ce5adca207e9a99433bb88
이경민 <cow970814@naver.com>
2024-03-22 23:05:56 +0900

    feat : Share Card Repository Firebase 저장 메서드 구현

--- iOS-Naduan/Source/Repository/ShareCardRepository.swift
+++ iOS-Naduan/Source/Repository/ShareCardRepository.swift
@@ -28,12 +28,12 @@ class ShareCardRepository {
     }
     
     session.didReceiveNotDecodableData = { [weak self] data in
-      do {
-        let card = try JSONDecoder().decode(BusinessCard.self, from: data)
-        completion(.success(card))
-      } catch {
+      guard let receivedCard = try? JSONDecoder().decode(BusinessCard.self, from: data) else {
         completion(.failure(ShareError.notFindReceiveData))
+        return
       }
+      
+      self?.saveCardData(sendCard: card, receivedCard: receivedCard, completion: completion)
     }
     
     self.multiPeerSession = session
@@ -41,82 +41,21 @@ class ShareCardRepository {
   }
 }
 
-//private extension ShareCardRepository {
-//  func configureMultiPeerSession(
-//    with max: Int,
-//    completion: @escaping ((Result<BusinessCard, Error>) -> Void)
-//  ) -> MultiPeerSession {
-//    let session = MultiPeerSession(service: "nadaun", identity: "com.share.nadaun", maxPeers: 1)
-//    
-//    session.peerConnectedHandler = { [weak self] in
-//      try? self?.nearBySession.connectToPeer(peer: $0)
-//    }
-//    
-//    session.peerDataHandler = { [weak self] in
-//      if let card = try? JSONDecoder().decode(BusinessCard.self, from: $0) {
-//        self?.saveCardDataWithTransaction(to: card, completion: completion)
-//        return
-//      }
-//      
-//      try? self?.nearBySession.didReceivePeerToken(data: $0, peer: $1)
-//    }
-//    
-//    session.peerDisconnectedHandler = { [weak self] in
-//      self?.nearBySession.disconnectPeer(peer: $0)
-//    }
-//    return session
-//  }
-//  
-//  func configureNearBySession(with card: BusinessCard) {
-//    nearBySession.didReachNear = { [weak self, card] in
-//      guard let data = try? JSONEncoder().encode(card) else { return }
-//      self?.multiPeerSession?.sendDataToAllPeers(data: data)
-//    }
-//    
-//    nearBySession.shareMyToken = { [weak self] myToken in
-//      guard let encodedData = try? NSKeyedArchiver.archivedData(
-//        withRootObject: myToken,
-//        requiringSecureCoding: true
-//      ) else { return }
-//      
-//      self?.multiPeerSession?.sendDataToAllPeers(data: encodedData)
-//    }
-//  }
-//}
-//
-//// MARK: FireStore Data Handling Method
-//private extension ShareCardRepository {
-//  func saveCardDataWithTransaction(
-//    to card: BusinessCard,
-//    completion: @escaping (Result<BusinessCard, Error>) -> Void
-//  ) {
-//    let document = store.document(card.cardID)
-//    let relationCollection = Firestore.firestore().collection("Relation")
-//    
-//    Firestore.firestore().runTransaction { (transaction, pointer) -> Any? in
-//      let document = try? transaction.getDocument(document)
-//      guard let receivedCard = try? document?.data(as: BusinessCard.self) else {
-//        pointer?.pointee = NSError(domain: "ReceiveError", code: -1)
-//        return nil
-//      }
-//      
-//      let relationDocument = relationCollection.document(card.userID)
-//        .collection(receivedCard.cardID).document()
-//      
-//      let _ = try? transaction.setData(from: receivedCard, forDocument: relationDocument)
-//      
-//      return receivedCard
-//    } completion: { receivedCard, error in
-//      if let error = error {
-//        completion(.failure(error))
-//        return
-//      }
-//      guard let receivedCard = receivedCard as? BusinessCard else {
-//        completion(.failure(ShareError.notFindReceiveData))
-//        return
-//      }
-//      
-//      completion(.success(receivedCard))
-//    }
-//  }
-//}
+private extension ShareCardRepository {
+  func saveCardData(
+    sendCard: BusinessCard,
+    receivedCard: BusinessCard,
+    completion: @escaping (Result<BusinessCard, Error>) -> Void
+  ) {
+    try? store.collection("Relation").document(sendCard.userID)
+      .collection(receivedCard.cardID)
+      .document().setData(from: receivedCard) { error in
+        if error != nil {
+          completion(.failure(ShareError.notFindReceiveData))
+          return
+        }
+        
+        completion(.success(receivedCard))
+      }
+  }
+}
